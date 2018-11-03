e1d4e7ae3340c730af853e9ac2349d42264d884b
Zanges <zanges@users.noreply.github.com>
2018-11-03 17:31:38 +0100

    Functional

--- UnitFrames.lua
+++ UnitFrames.lua
@@ -13,6 +13,7 @@ local playerClass = addonTable.playerClass
 local buttonIndex = 0
 local HealButtonsPlayer = {}
 local HealButtonsParty = {}
+local HealButtonsRaid = {}
 
 
 function UnitFramesModule:OnInitialize()
@@ -42,7 +43,7 @@ local UnitFramesStyle = function(self, unit)
     self:SetBackdrop(Media.Assets.BACKDROP)
     self:SetBackdropBorderColor(Media:Grayscale(0.4))
     self:SetBackdropColor(Media:Grayscale(0.15))
-    self:SetSize(ZHeal.db.profile.unitWidth, ((ZHeal.db.profile.unitWidth + ZHeal.db.profile.buttonSpacing) * ZHeal.db.class.numButtons))
+    self:SetSize(ZHeal.db.profile.unitWidth, (ZHeal.db.profile.unitWidth  * ZHeal.db.class.numButtons) - (ZHeal.db.profile.buttonSpacing  * (ZHeal.db.class.numButtons - 1)))
     local HealthBar, HealthBG = addonTable.AddVerticalHealthBar(self, self, "BOTTOMLEFT", 2, 0, 0.8, 1)
     local PowerBar, PowerBG = addonTable.AddVerticalPowerBar(self, self, "BOTTOMRIGHT", 2, 0, 0.2, 1)
 end
@@ -69,55 +70,61 @@ function UnitFramesModule:CreateUnitFrames()
         local UnitFramesParty = UnitFramesModule:CreateParty()
         UnitFramesParty:SetPoint("CENTER", addonName .. "_UnitFrames_PositionFrame", "CENTER", -40, 0)
     elseif groupType == "RAID" then
-        
+        local UnitFramesRaid = UnitFramesModule:CreateRaid()
+        UnitFramesRaid:SetPoint("CENTER", addonName .. "_UnitFrames_PositionFrame", "CENTER", -280, 0)
     end
 end
 
-function UnitFramesModule:CreateHealButton(Header, Index, Target, Parent)
-    local Size = ZHeal.db.profile.unitWidth
+function UnitFramesModule:CreateHealButton(Header, Index, Target, FadeTable, Parent)
     local Spacing = ZHeal.db.profile.buttonSpacing
+    local Size = ZHeal.db.profile.unitWidth - Spacing * 2
 
     local button = LAB:CreateButton(buttonIndex, "ZHeal_Button" .. buttonIndex, Header)
-    local y = (Size * (Index - 1)) + Spacing
+    local y = ((Size + Spacing) * (Index - 1)) + Spacing 
 
     button:SetPoint("BOTTOM", Parent, "BOTTOM", 0, y)
-    button:SetSize(Size - Spacing, Size - Spacing)
-    button:Show()
+    button:SetSize(Size, Size)
 
     button:SetAttribute("unit", Target)
 
     button:AddToMasque(LibStub("Masque"):Group(addonName, addonName))
     buttonIndex = buttonIndex + 1
+
+    table.insert(FadeTable, button)
+
     return button
 end
 
 function UnitFramesModule:CreatePlayer()
     local UnitFramesPlayer = oUF:Spawn("player", addonName .. "_UnitFrames_Player")
-    UnitFramesPlayer:SetScript("OnEnter", ZHeal_Player_OnEnter)
-    UnitFramesPlayer:SetScript("OnLeave", ZHeal_Player_OnLeave)
+    
+    local header = CreateFrame("Frame", "ZHeal_ButtonHeader_Player", UnitFramesPlayer, "SecureHandlerStateTemplate")
+    header:SetFrameLevel(8)
 
-    CreateFrame("Frame", "ZHeal_ButtonHeader_Player", UIParent, "SecureHandlerStateTemplate")
+    local FadeTable = {}
+    for i = 1, ZHeal.db.class.numButtons do
+        HealButtonsPlayer[i] = UnitFramesModule:CreateHealButton(header, i, "player", FadeTable, UnitFramesPlayer)
+    end
 
-    local limit = ZHeal.db.class.numButtons
-    if limit == nil then
-        limit = 5
-    end
-    for i = 1, limit do
-        HealButtonsPlayer[i] = UnitFramesModule:CreateHealButton(ZHeal_ButtonHeader_Player, i, "player", UnitFramesPlayer)
-    end
+    local FadeHandlerFrame = CreateFrame("Frame", "ZHeal_FadeHandlerFrame_Player", UnitFramesPlayer)
+    FadeHandlerFrame:SetAllPoints(UnitFramesPlayer)
+    FadeHandlerFrame:SetScript("OnUpdate", function(self)
+        if MouseIsOver(self) then
+            for _,b in ipairs(FadeTable) do
+                if not b:IsVisible() then
+                    b:Show()
+                end
+            end
+        else
+            for _,b in ipairs(FadeTable) do
+                if b:IsVisible() then
+                    b:Hide()
+                end
+            end
+        end
+    end)
+
     return UnitFramesPlayer
-end
-
-function ZHeal_Player_OnEnter(self)
-    for _,v in ipairs(HealButtonsPlayer) do
-        v:Show()
-    end
-end
-
-function ZHeal_Player_OnLeave(self)
-    for _,v in ipairs(HealButtonsPlayer) do
-        v:Hide()
-    end
 end
 
 function UnitFramesModule:CreateParty()
@@ -127,30 +134,91 @@ function UnitFramesModule:CreateParty()
 		"maxColumns", 4,
 		"unitsPerColumn", 1,
 		"columnAnchorPoint", "LEFT",
-		"columnSpacing", 0,
+		"columnSpacing", 2,
 		"oUF-initialConfigFunction", [[
-			self:SetWidth(120)
-			self:SetHeight(32)
+			
 		]]
 	)
 	party:SetPoint("CENTER", addonName .. "_UnitFrames_PositionFrame", "CENTER", 140, 0)
     
-    CreateFrame("Frame", "ZHeal_ButtonHeader_Party", UIParent, "SecureHandlerStateTemplate")
+    local header = CreateFrame("Frame", "ZHeal_ButtonHeader_Party", UnitFramesPlayer, "SecureHandlerStateTemplate")
+    header:SetFrameLevel(8)
 
-    local limit = ZHeal.db.class.numButtons
-    if limit == nil then
-        limit = 5
-    end
     for n = 1, 4 do
-        HealButtonsParty[n] = {}
         if _G["oUF_ZHealUnitFramesPartyUnitButton" .. n] ~= nil then
-            for i = 1, limit do
-                HealButtonsParty[n][i] = UnitFramesModule:CreateHealButton(ZHeal_ButtonHeader_Player, i, "party" .. n, "oUF_ZHealUnitFramesPartyUnitButton" .. n)
+            local FadeTable = {}
+            HealButtonsParty[n] = {}
+            for i = 1, ZHeal.db.class.numButtons do
+                HealButtonsParty[n][i] = UnitFramesModule:CreateHealButton(header, i, "party" .. n, FadeTable, "oUF_ZHealUnitFramesPartyUnitButton" .. n)
             end
+            local FadeHandlerFrame = CreateFrame("Frame", "ZHeal_FadeHandlerFrame_Party_" .. n, _G["oUF_ZHealUnitFramesPartyUnitButton" .. n])
+            FadeHandlerFrame:SetAllPoints("oUF_ZHealUnitFramesPartyUnitButton" .. n)
+            FadeHandlerFrame:SetScript("OnUpdate", function(self)
+                if MouseIsOver(self) then
+                    for _,b in ipairs(FadeTable) do
+                        if not b:IsVisible() then
+                            b:Show()
+                        end
+                    end
+                else
+                    for _,b in ipairs(FadeTable) do
+                        if b:IsVisible() then
+                            b:Hide()
+                        end
+                    end
+                end
+            end)
         end
     end
     
     return party
+end
+
+function UnitFramesModule:CreateRaid()
+    local raid = oUF:SpawnHeader(
+		nil, nil, "raid",
+		"showRaid", true,
+		"maxColumns", 40,
+		"unitsPerColumn", 1,
+		"columnAnchorPoint", "LEFT",
+		"columnSpacing", 2,
+		"oUF-initialConfigFunction", [[
+			
+		]]
+	)
+	raid:SetPoint("CENTER", addonName .. "_UnitFrames_PositionFrame", "CENTER", 140, 0)
+    
+    local header = CreateFrame("Frame", "ZHeal_ButtonHeader_Raid", UnitFramesPlayer, "SecureHandlerStateTemplate")
+    header:SetFrameLevel(8)
+
+    for n = 1, 4 do
+        if _G["oUF_ZHealUnitFramesRaidUnitButton" .. n] ~= nil then
+            local FadeTable = {}
+            HealButtonsRaid[n] = {}
+            for i = 1, ZHeal.db.class.numButtons do
+                HealButtonsRaid[n][i] = UnitFramesModule:CreateHealButton(header, i, "raid" .. n, FadeTable, "oUF_ZHealUnitFramesRaidUnitButton" .. n)
+            end
+            local FadeHandlerFrame = CreateFrame("Frame", "ZHeal_FadeHandlerFrame_Raid_" .. n, _G["oUF_ZHealUnitFramesRaidUnitButton" .. n])
+            FadeHandlerFrame:SetAllPoints("oUF_ZHealUnitFramesRaidUnitButton" .. n)
+            FadeHandlerFrame:SetScript("OnUpdate", function(self)
+                if MouseIsOver(self) then
+                    for _,b in ipairs(FadeTable) do
+                        if not b:IsVisible() then
+                            b:Show()
+                        end
+                    end
+                else
+                    for _,b in ipairs(FadeTable) do
+                        if b:IsVisible() then
+                            b:Hide()
+                        end
+                    end
+                end
+            end)
+        end
+    end
+    
+    return raid
 end
 
 function UnitFramesModule:GetHealButtonPlayerArray()
@@ -185,13 +253,19 @@ function UnitFramesModule:UpdateButtons(spellArray)
                     v:SetState(0, nil)
                 end
             end
-        else
-            
-        end
-        
+        end 
     end
-    
 
     -- raid
-
+    for n,t in ipairs(HealButtonsRaid) do
+        if t ~= nil then
+            for i,v in ipairs(HealButtonsRaid[n]) do
+                if spellArray[i] ~= nil then
+                    v:SetState(0, "spell", spellArray[i])
+                else
+                    v:SetState(0, nil)
+                end
+            end
+        end 
+    end
 end
